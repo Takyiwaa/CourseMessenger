96ecf552cbb77f6d3b1649565fa60826ecbdc7d9
Augustine Gyawu Adjei <austinejei@gmail.com>
2015-01-23 18:53:00 +0000

    added SMS engine

---
+++ CourseMessengerWeb/Controllers/SmsEngineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseMessengerWeb.Models;
+
+namespace CourseMessengerWeb.Controllers
+{
+    public class SmsEngineController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SmsEngineController()
+        {
+            _context = new ApplicationDbContext();
+            
+        }
+        // GET: SmsEngine
+        public ActionResult Run()
+        {
+
+            return View();
+        }
+    }
+}
